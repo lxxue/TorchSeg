83f4a80fac4c2e74f75e5354dc82cfe6f3a9b3b5
CoolPhilChen <chenhongjie@sjtu.edu.cn>
2020-07-19 20:29:10 +0800

    fix a bug
    remove `log_softmax` before cross-entropy

--- model/crfasrnn/network.py
+++ model/crfasrnn/network.py
@@ -28,8 +28,6 @@ class CrfRnnNet(nn.Module):
         # print("after crfrnn:", out.shape)    #debug
         
         if label is not None:
-            psp_fm = F.log_softmax(psp_fm, dim=1)
-            aux_fm = F.log_softmax(aux_fm, dim=1)
             psp_loss = self.criterion(psp_fm, label)
             aux_loss = self.criterion(aux_fm, label)
             crf_loss = self.criterion(out, label)
