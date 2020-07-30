76f0fbd2c6153315af6ca24cf4f7c62cc24a42d7
CoolPhilChen <chenhongjie@sjtu.edu.cn>
2020-07-30 23:53:39 +0200

    Add baselines

---
+++ baselines/tf_cnn_test.py
@@ -0,0 +1,175 @@
+import gzip
+import os
+import sys
+import urllib
+import matplotlib.image as mpimg
+from PIL import Image
+
+import tensorflow.python.platform
+import tensorflow as tf
+import sklearn.metrics
+
+import numpy
+import numpy as np
+
+import cv2
+from tqdm import tqdm
+
+NUM_CHANNELS = 3 # RGB images
+PIXEL_DEPTH = 255
+NUM_LABELS = 2
+TEST_SIZE = 10
+SEED = 66478  # Set to None for random seed.
+BATCH_SIZE = 16 # 64
+
+# Set image patch size in pixels
+# IMG_PATCH_SIZE should be a multiple of 4
+# image size should be an integer multiple of this number!
+# If you tweak this parameter, change the submission file accordingly
+IMG_PATCH_SIZE = 16
+
+test_dir = 'test_images/'
+
+# Extract patches from a given image
+def img_crop(im, w, h):
+    list_patches = []
+    imgwidth = im.shape[0]
+    imgheight = im.shape[1]
+    is_2d = len(im.shape) < 3
+    for i in range(0,imgheight,h):
+        for j in range(0,imgwidth,w):
+            if is_2d:
+                im_patch = im[j:j+w, i:i+h]
+            else:
+                im_patch = im[j:j+w, i:i+h, :]
+            list_patches.append(im_patch)
+    return list_patches
+
+# Convert array of labels to an image
+def label_to_img(imgwidth, imgheight, w, h, labels):
+    array_labels = numpy.zeros([imgwidth, imgheight])
+    idx = 0
+    for i in range(0,imgheight,h):
+        for j in range(0,imgwidth,w):
+            if labels[idx][0] > 0.5:
+                l = 0
+            else:
+                l = 1
+            array_labels[j:j+w, i:i+h] = l
+            idx = idx + 1
+    return array_labels
+
+def get_prediction(img):
+    data = numpy.asarray(img_crop(img, IMG_PATCH_SIZE, IMG_PATCH_SIZE))
+    data_node = tf.constant(data)
+    output = tf.nn.softmax(model(data_node))
+    output_prediction = s.run(output)
+    img_prediction = label_to_img(img.shape[0], img.shape[1], IMG_PATCH_SIZE, IMG_PATCH_SIZE, output_prediction)
+
+    return img_prediction
+
+if __name__ == '__main__':
+    conv1_weights = tf.Variable(
+        tf.truncated_normal([5, 5, NUM_CHANNELS, 32],  # 5x5 filter, depth 32.
+                            stddev=0.1,
+                            seed=SEED))
+    conv1_biases = tf.Variable(tf.zeros([32]))
+    conv2_weights = tf.Variable(
+        tf.truncated_normal([5, 5, 32, 64],
+                            stddev=0.1,
+                            seed=SEED))
+    conv2_biases = tf.Variable(tf.constant(0.1, shape=[64]))
+    fc1_weights = tf.Variable(  # fully connected, depth 512.
+        tf.truncated_normal([int(IMG_PATCH_SIZE / 4 * IMG_PATCH_SIZE / 4 * 64), 512],
+                            stddev=0.1,
+                            seed=SEED))
+    fc1_biases = tf.Variable(tf.constant(0.1, shape=[512]))
+    fc2_weights = tf.Variable(
+        tf.truncated_normal([512, NUM_LABELS],
+                            stddev=0.1,
+                            seed=SEED))
+    fc2_biases = tf.Variable(tf.constant(0.1, shape=[NUM_LABELS]))
+
+    def model(data, train=False):
+        """The Model definition."""
+        # 2D convolution, with 'SAME' padding (i.e. the output feature map has
+        # the same size as the input). Note that {strides} is a 4D array whose
+        # shape matches the data layout: [image index, y, x, depth].
+        conv = tf.nn.conv2d(data,
+                            conv1_weights,
+                            strides=[1, 1, 1, 1],
+                            padding='SAME')
+        # Bias and rectified linear non-linearity.
+        relu = tf.nn.relu(tf.nn.bias_add(conv, conv1_biases))
+        # Max pooling. The kernel size spec {ksize} also follows the layout of
+        # the data. Here we have a pooling window of 2, and a stride of 2.
+        pool = tf.nn.max_pool(relu,
+                              ksize=[1, 2, 2, 1],
+                              strides=[1, 2, 2, 1],
+                              padding='SAME')
+
+        conv2 = tf.nn.conv2d(pool,
+                            conv2_weights,
+                            strides=[1, 1, 1, 1],
+                            padding='SAME')
+        relu2 = tf.nn.relu(tf.nn.bias_add(conv2, conv2_biases))
+        pool2 = tf.nn.max_pool(relu2,
+                              ksize=[1, 2, 2, 1],
+                              strides=[1, 2, 2, 1],
+                              padding='SAME')
+
+        # Uncomment these lines to check the size of each layer
+        # print 'data ' + str(data.get_shape())
+        # print 'conv ' + str(conv.get_shape())
+        # print 'relu ' + str(relu.get_shape())
+        # print 'pool ' + str(pool.get_shape())
+        # print 'pool2 ' + str(pool2.get_shape())
+
+
+        # Reshape the feature map cuboid into a 2D matrix to feed it to the
+        # fully connected layers.
+        pool_shape = pool2.get_shape().as_list()
+        reshape = tf.reshape(
+            pool2,
+            [pool_shape[0], pool_shape[1] * pool_shape[2] * pool_shape[3]])
+        # Fully connected layer. Note that the '+' operation automatically
+        # broadcasts the biases.
+        hidden = tf.nn.relu(tf.matmul(reshape, fc1_weights) + fc1_biases)
+        # Add a 50% dropout during training only. Dropout also scales
+        # activations such that no rescaling is needed at evaluation time.
+        #if train:
+        #    hidden = tf.nn.dropout(hidden, 0.5, seed=SEED)
+        out = tf.matmul(hidden, fc2_weights) + fc2_biases
+
+        if train == True:
+            summary_id = '_0'
+            s_data = get_image_summary(data)
+            filter_summary0 = tf.summary.image('summary_data' + summary_id, s_data)
+            s_conv = get_image_summary(conv)
+            filter_summary2 = tf.summary.image('summary_conv' + summary_id, s_conv)
+            s_pool = get_image_summary(pool)
+            filter_summary3 = tf.summary.image('summary_pool' + summary_id, s_pool)
+            s_conv2 = get_image_summary(conv2)
+            filter_summary4 = tf.summary.image('summary_conv2' + summary_id, s_conv2)
+            s_pool2 = get_image_summary(pool2)
+            filter_summary5 = tf.summary.image('summary_pool2' + summary_id, s_pool2)
+
+        return out
+
+    test_files = os.listdir(test_dir)
+
+saver = tf.train.Saver()
+with tf.Session() as s:
+    saver.restore(s, "tmp/model.ckpt")
+    if not os.path.isdir('pred_cnn'):
+        os.mkdir('pred_cnn')
+    for img_fn in tqdm(test_files):
+        img_dir = test_dir + img_fn
+        if os.path.isfile(img_dir):
+            img = mpimg.imread(img_dir)
+        else:
+            print ('File ' + img_dir + ' does not exist')
+        img_prediction = get_prediction(img)
+        heatmap = (img_prediction * 255).astype(np.uint8)
+        cv2.imwrite("pred_cnn/"+img_fn, heatmap)
+
